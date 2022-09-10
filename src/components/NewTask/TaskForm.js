b2bfcb420682f3bbc3b7f7e94a20ca9d049eca28
Igor Magalhães <igortrapp30@gmail.com>
2022-09-10 11:21:13 -0400

    final

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,27 +1,24 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
-import classes from './TaskForm.module.css';
+import styles from "./TaskForm.module.css";
 
-const TaskForm = (props) =>
-{
+const TaskForm = (props) => {
   const taskInputRef = useRef();
 
-  const submitHandler = (event) =>
-  {
+  const submitHandler = (event) => {
     event.preventDefault();
 
     const enteredValue = taskInputRef.current.value;
 
-    if (enteredValue.trim().length > 0)
-    {
+    if (enteredValue.trim().length > 0) {
       props.onEnterTask(enteredValue);
     }
   };
 
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <input type='text' ref={taskInputRef} />
-      <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
+    <form className={styles.form} onSubmit={submitHandler}>
+      <input type="text" ref={taskInputRef} />
+      <button>{props.loading ? "Sending..." : "Add Task"}</button>
     </form>
   );
 };
